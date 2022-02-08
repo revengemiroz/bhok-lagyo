a3d6da18c15c2eaf171ff2463f437b8fa143f281
miroz devkota <mirozxy@gmail.com>
2022-02-08 23:03:56 +0545

    fix style issues

--- pages/result/index.js
+++ pages/result/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { ImgContainer, Title } from "./style";
+import { ImgContainer, Title } from "../../styles/Result/style";
 
 function Index(props) {
   return (
     <div>
       <Title className="title">Result</Title>
+
       <ImgContainer>
         <img
           src="https://i.ibb.co/HdDNR1r/20e71c10c4764d35be48e021b544ada3.jpg"
